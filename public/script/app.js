f4391f1310fd60e3db0bb9ae9971bb1a60b269fa
Dragunnai27 <shiroyume005@gmail.com>
2019-08-20 03:06:57 +0700

    add feature add task and remove all task

--- public/script/app.js
+++ public/script/app.js
@@ -5,41 +5,83 @@ console.log("app.js is running");
 //JSX - Javascript XML
 
 var app = {
-    name: 'To do list app',
+    name: 'TO DO LIST APP',
     subtitle: 'This is a simple app',
-    option: ['one', 'two']
+    option: []
 };
 
-var myApp = React.createElement(
-    'div',
-    null,
-    React.createElement(
-        'h1',
+var addTask = function addTask(e) {
+    e.preventDefault();
+
+    var task = e.target.elements.task.value;
+    if (task) {
+        app.option.push(task);
+        e.target.elements.task.value = '';
+    }
+    reRender();
+};
+var removeAll = function removeAll(e) {
+    e.preventDefault();
+    app.option = [];
+
+    reRender();
+};
+
+var reRender = function reRender() {
+    var myApp = React.createElement(
+        'div',
         null,
-        app.name
-    ),
-    React.createElement(
-        'h3',
-        null,
-        app.subtitle
-    ),
-    React.createElement(
-        'p',
-        null,
-        app.option ? 'List of work to do:' : ''
-    ),
-    React.createElement(
-        'ul',
-        null,
-        app.option.map(function (item) {
-            return React.createElement(
-                'li',
+        React.createElement(
+            'h2',
+            null,
+            app.name
+        ),
+        React.createElement(
+            'h3',
+            null,
+            app.subtitle
+        ),
+        React.createElement(
+            'p',
+            null,
+            app.option ? 'List of work to do:' : ''
+        ),
+        React.createElement(
+            'p',
+            null,
+            app.option.length
+        ),
+        React.createElement(
+            'button',
+            { onClick: removeAll },
+            'Remove all'
+        ),
+        React.createElement(
+            'ul',
+            null,
+            app.option.map(function (item) {
+                return React.createElement(
+                    'li',
+                    null,
+                    item
+                );
+            })
+        ),
+        React.createElement(
+            'form',
+            { onSubmit: addTask },
+            React.createElement('input', { type: 'text', name: 'task' }),
+            React.createElement(
+                'button',
                 null,
-                item
-            );
-        })
-    )
-);
+                'Add task'
+            )
+        )
+    );
+    ReactDOM.render(myApp, appRoot);
+};
 
 var appRoot = document.getElementById('app');
-ReactDOM.render(myApp, appRoot);
+// ReactDOM.render(myApp, appRoot);
+
+reRender();
--- src/app.js
+++ src/app.js
@@ -3,25 +3,54 @@ console.log("app.js is running")
 //JSX - Javascript XML
 
 const app = {
-    name: 'To do list app',
+    name: 'TO DO LIST APP',
     subtitle: 'This is a simple app',
-    option: ['one', 'two']
+    option: []
 };
 
-var myApp = (
-    <div>
-        <h1>{app.name}</h1>
-        <h3>{app.subtitle}</h3>
-        <p>{app.option ? 'List of work to do:': ''}</p>
-        <ul>
-            {app.option.map((item) =>{
-                return <li>{item}</li>
-            })}
-        </ul>
-    </div>
+const addTask = (e) => {
+    e.preventDefault();
 
-);
+    const task = e.target.elements.task.value;
+    if (task) {
+        app.option.push(task);
+        e.target.elements.task.value = '';
+    }
+    reRender();
+}
+const removeAll = (e) => {
+    e.preventDefault();
+    app.option = [];
+
+    reRender();
+}
+
+const reRender = () => {
+    const myApp = (
+        <div>
+            <h2>{app.name}</h2>
+            <h3>{app.subtitle}</h3>
+            <p>{app.option ? 'List of work to do:' : ''}</p>
+            <p>{app.option.length}</p>
+            <button onClick={removeAll}>Remove all</button>
+            <ul>
+                {app.option.map((item) => {
+                    return <li>{item}</li>
+                })}
+            </ul>
+            <form onSubmit={addTask}>
+                <input type='text' name='task'></input>
+                <button>Add task</button>
+            </form>
+        </div>
+    );
+    ReactDOM.render(myApp, appRoot);
+};
+
+const appRoot = document.getElementById('app');
+// ReactDOM.render(myApp, appRoot);
+
+reRender();
 
 
-var appRoot = document.getElementById('app');
-ReactDOM.render(myApp, appRoot);
+
